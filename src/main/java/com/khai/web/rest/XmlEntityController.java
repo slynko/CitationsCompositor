8705b47ef0a6fdb2f51a37675580c115dce954c5
slynko <oleksandr.o.slynko@gmail.com>
2016-12-06 22:50:26 +0200

    simple rest endpoint

--- src/main/java/com/khai/web/rest/XmlEntityController.java
+++ src/main/java/com/khai/web/rest/XmlEntityController.java
@@ -4,12 +4,22 @@ import com.khai.model.Standard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class XmlEntityController {
     @RequestMapping("/entities")
-    public Standard index() {
-        Standard standard = new Standard();
-        standard.setTitle("Test title");
-        return standard;
+    public List<Standard> index() {
+        Standard standard1 = new Standard();
+        standard1.setTitle("Test title1");
+        Standard standard2 = new Standard();
+        standard2.setTitle("Test title2");
+
+        List<Standard> standards = new ArrayList<Standard>();
+        standards.add(standard1);
+        standards.add(standard2);
+
+        return standards;
     }
 }
