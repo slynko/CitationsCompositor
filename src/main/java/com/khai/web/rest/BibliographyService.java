6caba3e7353bbed4d937e18a3fd221f5bedd205e
Aleksandr Slynko <aslynko@connecture.com>
2016-12-08 16:52:40 +0200

    entities in list +1

--- src/main/java/com/khai/web/rest/BibliographyService.java
+++ src/main/java/com/khai/web/rest/BibliographyService.java
@@ -12,10 +12,12 @@ public class BibliographyService {
     public List<String> getEntities() {
         String formattedStringOne = "1 Author L.L. 1990";
         String formattedStringTwo = "2 Author B.B. 1555";
+        String formattedStringThree = "3 Author B.B. 1999";
 
         List<String> standards = new ArrayList<>();
         standards.add(formattedStringOne);
         standards.add(formattedStringTwo);
+        standards.add(formattedStringThree);
 
         return standards;
     }
