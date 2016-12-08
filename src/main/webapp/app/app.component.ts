40b6d4412b5da50f13d9230a1f2585dfa2c556ff
Aleksandr Slynko <aslynko@connecture.com>
2016-12-08 16:50:49 +0200

    got working ui service

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,23 +1,23 @@
 import {Component} from 'angular2/core';
-//import {BibliographyService} from './service/bibliography.service'
+import {BibliographyService} from './service/bibliography.service'
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/template/home.html',
-    //providers: [BibliographyService]
+    providers: [BibliographyService]
 })
 export class AppComponent {
-    // constructor(private _bibliographyService: BibliographyService){
-    //     this._bibliographyService = _bibliographyService;
-    // }
+     constructor(private _bibliographyService: BibliographyService){
+         this._bibliographyService = _bibliographyService;
+     }
 
     getAllBibliographies (){
-        // this.error = "";
-        // this.bibliographies = [];
-        // this._bibliographyService.getAllBibliographies()
-        //     .subscribe(
-        //         data => this.bibliographies = data,
-        //         error => this.error = "Region " + this.region + " is invalid."
-        //     );
+          this.error = "";
+          this.bibliographies = [];
+          this._bibliographyService.getAllBibliographies()
+              .subscribe(
+                  data => this.bibliographies = data,
+                  error => this.error = "Region " + this.region + " is invalid."
+              );
     }
 }
